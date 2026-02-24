dc73dfa85a068d85588016981b5730b17f309809
Beza-Oriyon <bezawitlulekal@gmail.com>
2026-02-25 01:09:13 +0300

    Full CRUD operations for Task

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -27,5 +27,58 @@ const createTask = async (req, res) => {
         res.status(400).json({ success: false, message: error.message });
     }
 };
- 
-export {getTasks, createTask};
+
+// @desc    Get a single task
+// @route   GET /api/tasks/:id
+const getTask = async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id); // Search DB by the ID in the URL
+
+        if (!task) {
+            return res.status(404).json({ success: false, message: 'Task not found' });
+        }
+
+        res.status(200).json({ success: true, data: task });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Server Error or Invalid ID format' });
+    }
+};
+
+// @desc    Update a task
+// @route   PUT /api/tasks/:id
+const updateTask = async (req, res) => {
+    try {
+        // Find by ID and update it with the new data in req.body
+        // { new: true } tells Mongoose to return the updated version, not the old one
+        // { runValidators: true } forces it to check our blueprint rules again!
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!task) {
+            return res.status(404).json({ success: false, message: 'Task not found' });
+        }
+
+        res.status(200).json({ success: true, data: task });
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message });
+    }
+};
+
+// @desc    Delete a task
+// @route   DELETE /api/tasks/:id
+const deleteTask = async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if (!task) {
+            return res.status(404).json({ success: false, message: 'Task not found' });
+        }
+
+        res.status(200).json({ success: true, message: 'Task deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Server Error' });
+    }
+};
+export {getTasks, createTask, getTask, updateTask, deleteTask};
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createTask, getTasks} from '../controllers/taskController.js';
+import { createTask, getTasks, getTask, updateTask, deleteTask} from '../controllers/taskController.js';
 
 const router =  express.Router();
 
 router.route('/').get(getTasks).post(createTask);
+router.route('/:id').get(getTask).put(updateTask).delete(deleteTask);
 
 export default router;
