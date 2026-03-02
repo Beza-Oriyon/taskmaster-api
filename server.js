7df2ecfeeca2025f5ed2f0e711e346013579dacb
Beza-Oriyon <bezawitlulekal@gmail.com>
2026-03-03 01:28:01 +0300

    Added production security (Helmet, CORS, Rate Limiting, Sanitization)

--- server.js
+++ server.js
@@ -3,12 +3,47 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import taskRoutes from './routes/taskRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import helmet from 'helmet';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import mongoSanitize from 'express-mongo-sanitize';
 dotenv.config();// Load environment variables from .env file
 
 connectDB();// Connect to the database
 const app = express();
 
 app.use(express.json());// Allows express app to accept JSON data from requests!. Without this req.body will be undefined.
+
+// --- 2. EQUIP THE SECURITY MIDDLEWARE ---
+
+// Helmet adds extra security headers to our responses to hide server info from hackers
+app.use(helmet());
+
+// CORS allows frontend applications (like React/Vue) to communicate with our API
+app.use(cors());
+
+// MongoSanitize scrubs incoming data to prevent NoSQL Injection attacks
+app.use((req, res, next) => {
+    Object.defineProperty(req, 'query', {
+        value: { ...req.query },
+        writable: true,
+        configurable: true,
+        enumerable: true,
+    });
+    next();
+});
+app.use(mongoSanitize());
+
+// Rate Limiting: Prevent spam/brute force attacks
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 5, // Limit each IP to 100 requests per 10 minutes
+    message: { success: false, message: 'Too many requests, please try again later.' }
+});
+// Apply the rate limiter to all routes
+app.use(limiter);
+
+
 app.use('/api/tasks', taskRoutes);// Tells the app to use those routes for any URL that starts with /api/tasks
 app.use('/api/auth', authRoutes);
 
